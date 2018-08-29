d6c89e95edb55a595eb6b146c97bf002b971f0f4
InnaYugay <yuinnaa@gmail.com>
2018-08-29 15:31:30 -0400

    jumping and collision works

--- script.js
+++ script.js
@@ -12,60 +12,55 @@ window.onload = function() {
   }
   
   // collisionCheck(){
-    
-  //     this.blocks.forEach((oneBlock)=>{
-  //       // console.log(this.character.y + this.character.height)
-  //       // console.log("block's y: " + this.blocks[11].y);
-  //       // console.log("block's x: " + this.blocks[11].x);
-  //       // console.log("block's width" + this.blocks[11].width)
-  //       // console.log("block's x plus width: " + (this.blocks[11].x + this.blocks[11].width));
-  //       // console.log("char's x: " + this.character.x)
+  //   this.blocks.forEach((eachObstacle)=>{
+          
+  //       if((this.character.x + this.character.width >= eachObstacle.x &&      this.character.x <= eachObstacle.x+eachObstacle.width) &&
+  //       (this.character.y + this.character.height <= eachObstacle.y)){
+  //        // console.log("AHHHHHH collision!=-=-=--=-=-=-=-=-=-=-=-")
+  //         this.character.y = eachObstacle.y - this.character.height;
+  //         this.character.onTheBlock = true;
+  //           setTimeout(()=>{
+  //           this.character.onTheBlock = false
 
-  //       // console.log("character's y: " + this.character.y); 
-  //       // console.log("character's height: " + this.character.height);
-      
-  
-  //       if((this.character.y + this.character.height === oneBlock.y) && 
-  //         (oneBlock.x) < this.character.x && (this.character.x < (oneBlock.x + oneBlock.width))){
-  //           this.character.y = oneBlock.y - this.character.height;
-  //           console.log("=-=-=-=-=-=-====-=-=-==AHHHHHH collision!=-=-=--=-=-=-=-=-=-=-=-")
+  //           },750)
+  //        console.log(this.character.upLimit)
+  //        this.character.upLimit = this.character.y - 50;
+  //        // console.log('where am I')
   //       }
+  //       else{
+  //         if(this.character.onTheBlock === false){
+  //          this.character.upLimit = 500;
 
-  //       // if((this.character.y + this.character.height === this.blocks[11].y) && 
-  //       //   (this.blocks[11].x) < this.character.x && (this.character.x < (this.blocks[11].x + this.blocks[11].width))){
-  //       //   console.log("==-=-=-=-=-=-=-=-=-=-=-=-=-AHHHHHH collision!=-=-=--=-=-=-=-=-=-=-=-")
-  //       // }
-
-
-  //   })
-  //   // console.log(this.character.y + this.character.height)
-
-  //   // console.log("block y: " + this.blocks[9].y, "char y: " + this.character.y + this.character.height)
-
+  //         }
+  //       }
+    
+  //        })
   // }
 
-
   collisionCheck(){
+    this.blocks.forEach((eachObstacle)=>{
 
-    // if(this.character.x < this.blocks.x + this.blocks.width &&
-    // this.character.x + this.character.width > this.blocks.x &&
-    // this.character.x < this.blocks.y + this.blocks.height &&
-    // this.character.y + this.character.height > this.blocks.y) {
-    //   consle.log('Collision Detected')
-    // }
-    
-    
-         this.blocks.forEach((eachObstacle)=>{
-    
-                 if((this.character.x + this.character.width >= eachObstacle.x && this.character.x <= eachObstacle.x+eachObstacle.width) &&
-                 (this.character.y + this.character.height >= eachObstacle.y && this.character.y <= eachObstacle.y+eachObstacle.height)){
-                   // console.log("AHHHHHH collision!=-=-=--=-=-=-=-=-=-=-=-")
-                   this.character.y = eachObstacle.y - this.character.height;
-                   // console.log('where am I')
-                 }
-    
-         })
-  }
+            if((this.character.x + this.character.width >= eachObstacle.x && this.character.x <= eachObstacle.x+eachObstacle.width) &&
+            (this.character.y + this.character.height >= eachObstacle.y && this.character.y + this.character.height <= eachObstacle.y+eachObstacle.height)){
+
+              this.character.y = eachObstacle.y - this.character.height;
+              // this.character.y += 5;
+              this.character.onTheBlock = true;
+              setTimeout(()=> {
+              this.character.onTheBlock = false;
+              }, 350)
+              this.character.upLimit = this.character.y - 50;
+            }
+            else {
+              if(this.character.onTheBlock === false) {
+                this.character.upLimit =500;
+              }
+            }
+
+    })
+}
+
+
 
   pointsCollector(){}
 
@@ -85,13 +80,7 @@ window.onload = function() {
 
 ///////////////////////  CHARACTER CLASS //////////////////////
  class Character{
-  // constructor(){
-  // this.x = 200,
-  // this.y = 600,
-  // this.width = 100,
-  // this.height = 80
-  // this.imageSource = "../images/nick-on-head.png"
-  // }
+
   constructor(){
   this.height = 52,
   this.jumping = true,
@@ -100,6 +89,8 @@ window.onload = function() {
   this.x_velocity= 20,
   this.y= 550,
   this.y_velocity = 0
+  this.upLimit;
+  this.onTheBlock = false;
   this.imageSource = "../images/nick-on-head.png"
   // this.ctx = document.getElementById("theCanvas").getContext('2d');
   }
@@ -108,19 +99,13 @@ window.onload = function() {
 
  drawCharacter(){
   
-  // console.log(this)
-  
   const theImage = new Image();
   theImage.src = this.imageSource;
-  
-  // theImage.onload = (() => {
-    ctx.drawImage(theImage, this.x, this.y, this.width, this.height);
-    // console.log("drawing character")
-  // })
+  ctx.drawImage(theImage, this.x, this.y, this.width, this.height);
+   
  }
 
  moveAround(number){
-  // this.ctx.clearRect(this.x, this.y, this.width, this.height)
   switch(number){
     case 37: //left key
     if(this.x > 20){
@@ -134,101 +119,40 @@ window.onload = function() {
     }
     break;
     default:
-    // console.log("are you lost?")
-
-
+    
   }
-  // this.drawCharacter();
-
+ 
  }
 
   
-
-
-  
-
-  //  controller(){
-  //     var left = false;
-  //     var right = false;
-  //     var up = false;
-  
-  //       setInterval(()=>{
-  //         clearInterval(theInt);
-  //         clearInterval(otherInt);
-  //         clearInterval(firstInt);
-  //       if(this.y > 50){
-          
-  //         this.y -= 50;
-         
-
-  //         theInt = setInterval(() => {
-  //         if(this.y <= 550){
-  //             this.y += 5;
-  //           }
-  //         }, 50)
-  //       } else {
-  //         this.y = 50;
-  //         /
-  //         otherInt = setInterval(() => {
-  //           if(this.y <= 550){
-              
-  //             this.y += 5;
-  //             // this.drawCharacter()
-  //           }
-  //         }, 50)  
-  //       }
-  //     }, 510)
-  //     ;
-  //   }
-  
    update(){
-  
-    var upLimit = this.y;
+
+    this.upLimit = 500;
     var jumpUp = false;
     var jumpDown = false;
-    // var a = false;
+
     
     setInterval(()=>{
-    if(this.y >= 550){
+      if(this.y >= this.upLimit + 50){
       jumpUp = true;
       jumpDown = false;
     } 
-    if (this.y <= 500){
+    if (this.y <= this.upLimit){
       jumpDown = true;
       jumpUp = false;
     }
-    
-      if(jumpUp === true){
-        // console.log("moving up");
-        this.y -= 2;
-        // console.log(this.y)
+      if(jumpUp === true){     
+        this.y -= 5;    
      }
       else{
-        // this.y += this.y_velocity;
-        this.y += 2;
-        // consoles.log(this.y)
-        // console.log("moving down");
+        this.y += 5;
       }
     }, 50)
 
   }
 
+}
 
-
-
-  //  controlJump(){
-  //    setInterval(()=>{
-  //     console.log("control jump")
-  //     this.update();
-  //    }, 100)
-  //  }
-  }
-
-    
-
-  
-  
-  
 
 ///////////////////////  BLOCKS CLASS //////////////////////
 
@@ -266,17 +190,14 @@ window.onload = function() {
       }
      
        if(moveToTheRight){
-        this.x += 10;
+        this.x += 5;
       }
         else{
-        this.x -= 10;
+        this.x -= 5;
         // console.log(this.x)
         
       }
-
-
-      //  this.blocksDraw();
-    },100)
+    },50)
   } 
   }
   
@@ -286,16 +207,10 @@ window.onload = function() {
     newGame = new HeadJump();
     newGame.character = new Character;
     newGame.character.drawCharacter();
-    // newGame.character.jump();
-    // setInterval(newGame.character.jump(),20);
     newGame.character.moveAround();
 
 
     newGame.blocks.push(new Blocks(100,100),new Blocks (200,140), new Blocks (480,180), new Blocks (150,220), new Blocks(400,260), new Blocks(450,300),new Blocks (210,340),new Blocks (300,380),new Blocks (250,420),new Blocks (500,460), new Blocks (50,500),new Blocks (350,554));
-
-    // for(i=0; i< newGame.blocks.length; i++){
-    //   newGame.blocks[i].blocksDraw();
-    // }
 
     for(i=0; i< newGame.blocks.length; i++){
       newGame.blocks[i].blocksMoveRandom();
